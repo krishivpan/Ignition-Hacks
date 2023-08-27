8758555c903e3befd0aed6ed70e0864f9871c6da
krishivpan <103297164+krishivpan@users.noreply.github.com>
2023-08-27 16:21:20 -0400

    Update app.py
    there are no more errors but it doesn't print anything

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import json
-import bson
+from bson import json_util
 import requests
 import googlemaps
 from datetime import datetime
@@ -18,8 +18,8 @@ def send_locations():
 
         data = request.get_data
         print(data)
-        start_location = data['startLocation']
-        end_location = data['endLocation']
+        start_location = data(['startLocation'])
+        end_location = data(['endLocation'])
 
         # Print the received locations
         print("Received start location:", start_location)
@@ -42,31 +42,31 @@ def send_locations():
         if origin_result and destination_result:
             origin = origin_result[0]['geometry']['location']
             destination = destination_result[0]['geometry']['location']
+
+            # Get directions and details
+            biking_result = gmaps_client.directions(origin, destination, mode="bicycling", avoid='ferries', departure_time="now")[0]['legs'][0]
+            transit_result = gmaps_client.directions(origin, destination, mode="transit", avoid='ferries', departure_time="now")[0]['legs'][0]
+            driving_result = gmaps_client.directions(origin, destination, mode="driving", avoid='ferries', departure_time="now")[0]['legs'][0]
+
+            # Print distance and duration details
+            print("Biking:")
+            print("Distance:", biking_result['distance']['text'])
+            print("Duration:", biking_result['duration']['text'])
+
+            print("Transit:")
+            print("Distance:", transit_result['distance']['text'])
+            print("Duration:", transit_result['duration']['text'])
+
+            print("Driving:")
+            print("Distance:", driving_result['distance']['text'])
+            print("Duration:", driving_result['duration']['text'])
         else:
             print("Invalid addresses.")
 
-        # Get directions and details
-        biking_result = gmaps_client.directions(origin, destination, mode="bicycling", avoid='ferries', departure_time="now")[0]['legs'][0]
-        transit_result = gmaps_client.directions(origin, destination, mode="transit", avoid='ferries', departure_time="now")[0]['legs'][0]
-        driving_result = gmaps_client.directions(origin, destination, mode="driving", avoid='ferries', departure_time="now")[0]['legs'][0]
-
-        # Print distance and duration details
-        print("Biking:")
-        print("Distance:", biking_result['distance']['text'])
-        print("Duration:", biking_result['duration']['text'])
-
-        print("Transit:")
-        print("Distance:", transit_result['distance']['text'])
-        print("Duration:", transit_result['duration']['text'])
-
-        print("Driving:")
-        print("Distance:", driving_result['distance']['text'])
-        print("Duration:", driving_result['duration']['text'])
-
-
         response = {'message': 'Data received successfully'}
         print("hi")
-        return json.loads(bson.json_util.dumps(response))
+        return json.loads(json_util.dumps(response))
+    
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}                            
 
 if __name__ == '__main__':
