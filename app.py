3d6704037b469f6ac57c12109ffad1ae4f9aee3a
krishivpan <103297164+krishivpan@users.noreply.github.com>
2023-08-27 00:19:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/send-locations', methods=['POST'])
-def receive_locations():
-    data = request.get_json()
-    start_location = data.get('startLocation')
-    end_location = data.get('endLocation')
+def send_locations():
+    data = request.json
 
-    # You can save the locations to a JSON file, process them, or perform any other action
-    # For example, you can save them to a file named 'locations.json'
-    with open('locations.json', 'w') as file:
-        json.dump(data, file)
+    start_location = data['startLocation']
+    end_location = data['endLocation']
 
-    return jsonify({'message': 'Locations received by Flask'})
+    # Connect to the SQLite database
+    connection = sqlite3.connect('locations.db')
+    cursor = connection.cursor()
+
+    # Insert the locations into the database
+    cursor.execute("INSERT INTO locations (start_location, end_location) VALUES (?, ?)",
+                   (start_location, end_location))
+    
+    connection.commit()
+    connection.close()
+
+    return jsonify({'message': 'Locations saved successfully'})
 
 if __name__ == '__main__':
     app.run()
